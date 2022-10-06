dfc0bca88e021abe5a22abb6dcecfde196c2cc01
Jay <jaymontojo@Jays-Mac-mini.local>
2022-10-07 03:27:16 +0900

    feat: initialize a simple express server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.json()); //MIDDLEWARE
+
+app.get('/hello', (req, res) => {
+  res.send('world🌎')
+    .status(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`app is listening @ http://localhost:${4000}`);
+});
